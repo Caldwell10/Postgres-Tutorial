7c5ebae52cde217d53c783b337db3972e7d97cdd
Caldwell10 <caldwell.kibe@strathmore.edu>
2025-09-21 22:39:19 +0300

    Created an update script to modify a value in a tuple

---
+++ update.py
@@ -0,0 +1,31 @@
+import psycopg2
+from config import load_config
+
+def update_vendor(vendor_id, vendor_name):
+    """Update a vendor's name based on the vendor_id"""
+    updated_row_count = 0
+
+    query = """UPDATE vendors
+             SET vendor_name = %s
+             WHERE vendor_id = %s;"""
+    
+    config = load_config()
+    try:
+        with psycopg2.connect(**config) as conn:
+            with conn.cursor() as cur:
+                # execute the UPDATE statement
+                cur.execute(query, (vendor_name,vendor_id))
+
+                updated_row_count = cur.rowcount
+
+            # commit the changes to the database
+            conn.commit()
+    except (psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        return updated_row_count
+              
+if __name__ == '__main__':
+    update_vendor(1, 'IBM Corporation')
+    print("Vendor updated successfully")
+    
